a561dab2346acb957024741766efe489c0032629
gehadHassanein <gehadelmenshawy2@gmail.com>
2024-08-31 23:54:09 +0300

    Fixing an error

--- fdisk/fdisk.c
+++ fdisk/fdisk.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <inttypes.h>
 
+#define SECTOR_SIZE 512
+#define PARTITION_TABLE_SIZE 66 
+#define BOOTSTRAP_CODE_AREA 446
+#define NUM_PARTITIONS 4
+#define EXTENDED 0x05
 
-#define SECTOR_SIZE (512)
-#define PARTITION_TABLE_SIZE (66) 
-#define BOOTSTRAP_CODE_AREA (446)
-#define NUM_PARTITIONS (4)
-#define EXTENDED (0x05)
 typedef struct {
     uint8_t status;
     uint8_t first_chs[3];
@@ -18,49 +19,55 @@ typedef struct {
     uint8_t last_chs[3];
     uint32_t lba;
     uint32_t sectorCount;
-}PartitionEntry_t;
+} PartitionEntry_t;
 
-int main(int argc, char** argv) {
-    uint8_t buff[PARTITION_TABLE_SIZE];
-    int32_t fd;
-    if (argc == 2) {
-        if ((fd = open(argv[1], O_RDONLY) == -1)) {
-            perror("open");
-        }
-    } else {
-        perror("argc");
+int main(int argc, char **argv) {
+    uint8_t buff[SECTOR_SIZE];
+    int fd;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <device>\n", argv[0]);
+        return 1;
     }
 
-    off_t offset = lseek(fd, BOOTSTRAP_CODE_AREA, SEEK_SET);
-    ssize_t bytes = read(fd, buff, PARTITION_TABLE_SIZE);
-    if (bytes != PARTITION_TABLE_SIZE) {
-        perror("read");
+    if ((fd = open(argv[1], O_RDONLY)) == -1) {
+        perror("Error opening device");
+        return 1;
     }
 
-    PartitionEntry_t *pPartitionEntry = buf;
+    if (read(fd, buff, SECTOR_SIZE) != SECTOR_SIZE) {
+        perror("Error reading sector");
+        close(fd);
+        return 1;
+    }
 
-    printf("%-5s %-10s        %-10s %-10s %-10s %-10s %-10s %-10s\n",
-	   "Device", "Boot", "Start", "End", "Sectors", "Size", "Id",
-	   "Type");
+    close(fd);
 
-    for (int32_t iterator = 0; iterator < NUM_PARTITIONS; iterator++) {
-        if (pPartitionEntry[i].partitionType == 0) {
+    PartitionEntry_t *pPartitionEntry = (PartitionEntry_t *) &buff[BOOTSTRAP_CODE_AREA];
+
+    printf("%-10s %-5s %-10s %-10s %-10s %-10s %-10s\n",
+           "Device", "Boot", "Start", "End", "Sectors", "Size (MB)", "Id");
+
+    for (int iterator = 0; iterator < NUM_PARTITIONS; iterator++) {
+        if (pPartitionEntry[iterator].partitionType == 0) {
             continue; // Skip empty entries
         }
-        printf("%s%-5d %-10c %-10u %-10u %-10u %uM      %-10X\n",
-            argv[1],
-            iterator + 1,
-            pPartitionEntry[i].status == 0x80 ? '*' : ' ';
-            pPartitionEntry[i].lba,
-	        pPartitionEntry[i].lba + pPartitionEntry[i].sectorCount - 1, 
-            pPartitionEntry[i].sectorCount,
-            (uint32_t) (((uint64_t) pPartitionEntry[i].sectorCount *
-			    512) / (1024 * 1024 * 1024)),
-            pPartitionEntry[i].partitionType);
 
-	    if (pPartitionEntry[i].partitionType == EXTENDED) {              
-	        ReadExtended(argv, pPartitionEntry[i].lba);
-	    }   
+        uint64_t sizeInMB = ((uint64_t) pPartitionEntry[iterator].sectorCount * SECTOR_SIZE) / (1024 * 1024);
+
+        printf("%s%-3d %-5c %-10u %-10u %-10u %-10u %-10X\n",
+               argv[1],
+               iterator + 1,
+               pPartitionEntry[iterator].status == 0x80 ? '*' : ' ',
+               pPartitionEntry[iterator].lba,
+               pPartitionEntry[iterator].lba + pPartitionEntry[iterator].sectorCount - 1,
+               pPartitionEntry[iterator].sectorCount,
+               (uint32_t) ((uint64_t) pPartitionEntry[iterator].sectorCount * SECTOR_SIZE) / (1024 * 1024),
+               pPartitionEntry[iterator].partitionType);
+
+        if (pPartitionEntry[iterator].partitionType == EXTENDED) {
+            // ReadExtended(argv, pPartitionEntry[iterator].lba); 
+        }
     }
 
     return 0;
